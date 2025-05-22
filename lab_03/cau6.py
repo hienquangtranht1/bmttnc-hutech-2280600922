ab976978c4ad91b261f702c790be1586c8cf9a07
Trần Quang Hiển <160805012+hienquangtranht1@users.noreply.github.com>
2025-05-22 12:51:14 +0700

    Add files via upload

---
+++ lab_03/cau6.py
@@ -0,0 +1,16 @@
+def xoa_phan_tu(dictionary, key):
+    if key in dictionary:
+        del dictionary[key]
+        return True
+    else:
+        return False
+
+# Using the function and printing results
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+key_to_delete = 'b'
+result = xoa_phan_tu(my_dict, key_to_delete)
+
+if result:
+    print("Element has been deleted from Dictionary:", my_dict)
+else:
+    print("Element not found in Dictionary.")
